2e745bc630117f3fdecef676b7c1ccbe03bded5b
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-04-21 09:47:35 +0600

    hero section done

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -5,7 +5,7 @@ const CommonLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Navbar />
-      {children}
+      <div className="min-h-screen">{children}</div>
       <Footer />
     </div>
   );
--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,10 @@
+import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
 import { Button } from "@mui/material";
 
 const HomePage = () => {
   return (
     <div>
-      <h2>this is home page</h2>
-      <Button>Text</Button>
+      <HeroSection />
     </div>
   );
 };
