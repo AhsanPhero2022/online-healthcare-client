df2f6c553efb9de90eed079b20c60db6cd39e312
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-04-21 06:34:47 +0600

    configured mui theme provider and components buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@mui/material";
+
 const HomePage = () => {
   return (
     <div>
       <h2>this is home page</h2>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 };
