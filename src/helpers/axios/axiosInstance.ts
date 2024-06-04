d2c6c30e00bbcdee4dd9e82f088003554792dbc7
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-06-04 08:20:16 +0600

    working on axios instance

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -8,10 +8,8 @@ instance.defaults.headers.post["Content-Type"] = "application/json";
 instance.defaults.headers["Accept"] = "application/json";
 instance.defaults.timeout = 60000;
 
-// Add a request interceptor
 instance.interceptors.request.use(
   function (config) {
-    // Do something before request is sent
     const accessToken = getFromLocalStorage(authKey);
 
     if (accessToken) {
@@ -20,7 +18,6 @@ instance.interceptors.request.use(
     return config;
   },
   function (error) {
-    // Do something with request error
     return Promise.reject(error);
   }
 );
@@ -29,8 +26,6 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   //@ts-ignore
   function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     const responseObject: ResponseSuccessType = {
       data: response?.data?.data,
       meta: response?.data?.meta,
@@ -38,14 +33,12 @@ instance.interceptors.response.use(
     return responseObject;
   },
   function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
     const responseObject: IGenericErrorResponse = {
       statusCode: error?.response?.data?.statusCode || 500,
       message: error?.response?.data?.message || "Something went wrong!!!",
       errorMessages: error?.response?.data?.message,
     };
-    // return Promise.reject(error);
+
     return responseObject;
   }
 );
